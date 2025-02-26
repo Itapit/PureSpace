9768f575df0932457f111daa4487c874b330123a
Itapit <itamardavid90@gmail.com>
2025-02-26 07:47:15 +0200

    hello

--- core/__init__.py
+++ core/__init__.py
@@ -1,15 +1,17 @@
-# # Import key functions from each module
-# from .file_operations import move_unwanted_files
-# from .media_organizer import organize_images, organize_videos
-# from .duplicate_handler import find_duplicates
-# from .cleanup import delete_empty_folders, delete_empty_files
+# Import key functions from each module
+from .helpers import ensure_directory_exists, is_folder_empty
+from .media_organizer import organize_media_by_date, move_media_duplicates, delete_duplicates_folders
+from .cleaner import delete_empty_files, delete_empty_folders, find_large_files, move_unwanted_files
 
-# # List of public functions accessible with `from core import *`
-# __all__ = [
-#     "move_unwanted_files",
-#     "organize_images",
-#     "organize_videos",
-#     "find_duplicates",
-#     "delete_empty_folders",
-#     "delete_empty_files"
-# ]
+# List of public functions accessible with `from core import *`
+__all__ = [
+    "ensure_directory_exists",
+    "is_folder_empty",
+    "organize_media_by_date",
+    "move_media_duplicates",
+    "delete_duplicates_folders",
+    "delete_empty_files",
+    "delete_empty_folders",
+    "find_large_files",
+    "move_unwanted_files"
+]
